51add3f1f87177fc9d167f27a4686430159fc0f9
Ken Bonny <github@ken.morethancode.be>
2025-07-25 15:47:35 +0200

    Add search functionality to FreezerManager
    - Introduced `SearchFreezerItems` component for item filtering.
    - Integrated item search with the FreezerManager's state and sorting logic.

--- src/components/freezerManager.tsx
+++ src/components/freezerManager.tsx
@@ -1,21 +1,25 @@
 ﻿import {AddFreezerItemForm} from "./addFreezerItemForm.tsx";
 import React, {useState} from "react";
 import {Unit, type FreezerItem} from "./models.ts";
+import {SearchFreezerItems} from "./searchFreezerItems.tsx";
 
 export const FreezerManager: React.FC = () => {
     const [freezerItems, setFreezerItems] = useState<FreezerItem[]>([]);
+    const [filteredItems, setFilteredItems] = useState<FreezerItem[]>([]);
 
     const handleAddItem = (newItem: FreezerItem) => {
         setFreezerItems(prev => [...prev, newItem]);
     };
 
-    const sortedItems = [...freezerItems].sort((a, b) =>
+    const sortedItems = [...filteredItems].sort((a, b) =>
         a.name.toLowerCase().localeCompare(b.name.toLowerCase())
     );
 
     return (
         <div>
             <AddFreezerItemForm onAddItem={handleAddItem} />
+            <hr />
+            <SearchFreezerItems items={freezerItems} onSearch={items => setFilteredItems(items)} />
             <table>
                 <thead>
                 <tr>
---
+++ src/components/searchFreezerItems.tsx
@@ -0,0 +1,33 @@
+﻿import type {FreezerItem} from "./models.ts";
+import {useEffect, useState} from "react";
+
+export interface SearchFreezerItemsProps{
+    items: FreezerItem[];
+    onSearch: (items: FreezerItem[]) => void;
+}
+
+export function SearchFreezerItems({items, onSearch}: SearchFreezerItemsProps) {
+    const [terms, setTerms] = useState<string>("");
+
+    useEffect(() => searchItems(terms), [items])
+
+    function handleNewSearchTerms(e: React.ChangeEvent<HTMLInputElement>) {
+        setTerms(e.target.value.toLowerCase());
+        searchItems(terms);
+    }
+
+    function searchItems(searchTerms: string) {
+        const filteredItems = [...items].filter((item: FreezerItem) => item.name.toLowerCase().includes(searchTerms));
+        onSearch(filteredItems);
+    }
+
+    return (
+        <form>
+            <input type="text"
+                   id="searchTerms"
+                   name="searchTerms"
+                   onChange={handleNewSearchTerms}
+                   placeholder="Search terms: steak..." />
+        </form>
+    );
+}
