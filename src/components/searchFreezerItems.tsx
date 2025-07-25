d8c44743901b303eb4fab27bb2977871ea95f587
Ken Bonny <github@ken.morethancode.be>
2025-07-25 18:22:52 +0200

    Add "Include deleted" filter to SearchFreezerItems component
    - Added `includeDeleted` checkbox for filtering results to include deleted items.
    - Updated `searchItems` logic to respect the `includeDeleted` state.
    - Adjusted `useEffect` and event handlers to account for the new filter.

--- src/components/searchFreezerItems.tsx
+++ src/components/searchFreezerItems.tsx
@@ -9,17 +9,26 @@ export interface SearchFreezerItemsProps{
 
 export function SearchFreezerItems({items, onSearch}: SearchFreezerItemsProps) {
     const [terms, setTerms] = useState<string>("");
+    const [includeDeleted, setIncludeDeleted] = useState(false);
     const debouncedSearchTerms = useDebounce(terms, 500);
 
-    useEffect(() => searchItems(debouncedSearchTerms), [debouncedSearchTerms, items])
+    useEffect(() => searchItems(terms, includeDeleted), [debouncedSearchTerms, items])
 
     function handleNewSearchTerms(e: React.ChangeEvent<HTMLInputElement>) {
-        setTerms(e.target.value.toLowerCase());
-        searchItems(terms);
+        const termsInput = e.target.value.toLowerCase();
+        setTerms(termsInput);
+        searchItems(termsInput, includeDeleted);
     }
 
-    function searchItems(searchTerms: string) {
-        const filteredItems = [...items].filter((item: FreezerItem) => item.name.toLowerCase().includes(searchTerms));
+    function handleIncludeDeleted(e: React.ChangeEvent<HTMLInputElement>) {
+        const includeDeletedCheckBox = e.target.checked;
+        setIncludeDeleted(includeDeletedCheckBox);
+        searchItems(terms, includeDeletedCheckBox);
+    }
+
+    function searchItems(searchTerms: string, includeDeleted: boolean) {
+        const filteredItems = [...items].filter((item: FreezerItem) => item.name.toLowerCase().includes(searchTerms.toLowerCase()))
+            .filter(item => includeDeleted ? true : !item.isDeleted);
         onSearch(filteredItems);
     }
 
@@ -30,6 +39,14 @@ export function SearchFreezerItems({items, onSearch}: SearchFreezerItemsProps) {
                    name="searchTerms"
                    onChange={handleNewSearchTerms}
                    placeholder="Search terms: steak..." />
+
+            <label>
+                <input type="checkbox"
+                       id="includeDeleted"
+                       name="includeDeleted"
+                       onChange={handleIncludeDeleted} />
+                Include deleted
+            </label>
         </form>
     );
 }
