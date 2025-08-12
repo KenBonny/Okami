4b0e93505a5ebdeae3271a3bfc7594da89893523
Ken Bonny <github@ken.morethancode.be>
2025-08-12 16:26:23 +0200

    Refine `FreezerManager` layout: enhance responsiveness with updated Tailwind margin classes for medium and large screens.

--- src/components/freezerManager.tsx
+++ src/components/freezerManager.tsx
@@ -65,7 +65,7 @@ export const FreezerManager: React.FC = () => {
     }
 
     return (
-        <div>
+        <div className="md:mx-24 lg:mx-64">
             <div className="flex flex-col-reverse lg:flex-row m-3">
                 <AddFreezerItemForm className="" onAddItem={handleAddItem} />
                 <GoogleAuth className="items-baseline-last lg:place-content-end" onSuccess={loadFreezerItems}  onLogout={logout} />
